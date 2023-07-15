1c44d156d14b0d74c6d2a4b91e70032727a5d392
Esther Martorell <esther.martorell@allianz.com>
2023-07-15 07:15:26 +0200

    :sparkles: create own observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +15,23 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     //This trigger a observal every x seconds
-    this.firstObsSubscription = interval(1000).subscribe( count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe( count => {
+    //   console.log(count);
+    // });
+    //Same before but creating our own observable:
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+        //observer.error()//Type of error
+        //observer.complete()//Different types, this sends a complete observer
+      }, 1000);
     });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    })
   }
 
   ngOnDestroy(): void {
